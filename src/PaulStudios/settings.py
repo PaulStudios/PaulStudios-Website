b8a7b239f5c7ffec8d18b741b4dfcf013d2435cf
HilFing <indradip.paul@outlook.com>
2024-05-12 17:48:03 +0530

    Update settings.py

--- src/PaulStudios/settings.py
+++ src/PaulStudios/settings.py
@@ -22,7 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env("SECRET_KEY")
+if 'test' in sys.argv:
+    SECRET_KEY = "56557s45NY&S$N5^$%n^$%s6n%bdty"
+else: 
+    SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -157,5 +160,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 import sys
 if 'test' in sys.argv:
-    SECRET_KEY = "56557s45NY&S$N5^$%n^$%s6n%bdty"
     DATABASES['default'] = DATABASES['test']
