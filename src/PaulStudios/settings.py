e511252ad886188b752122ea51486b51ccf6f5d0
HilFing <indradip.paul@outlook.com>
2024-05-12 20:03:54 +0530

    Update settings.py

--- src/PaulStudios/settings.py
+++ src/PaulStudios/settings.py
@@ -19,6 +19,9 @@ environ.Env.read_env()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+from django.core.management.commands.runserver import Command as runserver
+runserver.default_port = env("PORT")
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
